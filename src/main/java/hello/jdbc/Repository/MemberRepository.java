a9839766b51f97f5174f19def8a44edd7714c882
kystic125 <kystic125@naver.com>
2025-09-26 18:19:51 +0900

    체크 예외와 런타임 예외 적용

---
+++ src/main/java/hello/jdbc/Repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.jdbc.Repository;
+
+import hello.jdbc.domain.Member;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Member findById(String memberId);
+
+    void update(String memberId, int money);
+
+    void delete(String memberId);
+}
