390e12ecac9e4fd62e40d66bbaa422f45e750a83
kystic125 <kystic125@naver.com>
2025-09-29 17:37:35 +0900

    JdbcTemplate - Jdbc 반복 문제 해결

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -1,9 +1,6 @@
 package hello.jdbc.service;
 
-import hello.jdbc.Repository.MemberRepository;
-import hello.jdbc.Repository.MemberRepositoryV3;
-import hello.jdbc.Repository.MemberRepositoryV4_1;
-import hello.jdbc.Repository.MemberRepositoryV4_2;
+import hello.jdbc.Repository.*;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -52,7 +49,8 @@ class MemberServiceV4Test {
         @Bean
         MemberRepository memberRepository() {
 //            return new MemberRepositoryV4_1(dataSource);
-              return new MemberRepositoryV4_2(dataSource);
+//            return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource);
         }
 
         @Bean
